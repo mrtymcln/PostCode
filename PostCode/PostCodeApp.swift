6bc9f906af068f8d7b36cb37cdd19610345bb7f2
Martin McLean <mrtymcln@gmail.com>
2026-03-15 10:30:56 +1100

    Initial Commit

---
+++ PostCode/PostCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PostCodeApp.swift
+//  PostCode
+//
+//  Created by Marty McLean on 15/3/2026.
+//
+
+import SwiftUI
+
+@main
+struct PostCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
